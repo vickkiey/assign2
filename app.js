b32a9533d0ae5f0729e7451047b0dc815e3a0eb4
vickkiey <vickkiey0@hotmail.com>
2015-11-18 17:23:36 -0500

    working mostly

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 //mongoose
 var mongoose = require('mongoose');
-
+var passport = require('passport');
+var session = require ('express-session');
+var flash = require ('connect-flash');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 var articles = require('./routes/articles');
 //local mongoose
 mongoose.connect('mongodb://localhost/userDB');
@@ -24,6 +27,8 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+require('./config/passport')(passport);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -35,6 +40,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Session setup
+app.use(session({
+  secret: 'someSecret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// part of passport configuration
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+
+
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
-/* GET home page. */
+var User = require('../models/user');
+
+/* Render home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Victoria Gillis Portfolio' });
+  res.render('index', { title: 'Victoria Gillis Portfolio',
+    displayName: req.user ? req.user.displayName : ''
+    });
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About Me' });
@@ -17,5 +25,60 @@ router.get('/services', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
+
+
+/* Render Login page. */
+router.get('/login', function (req, res, next) {
+    if (!req.user) {
+        res.render('login', {
+            title: 'Login',
+          /*  messages: req.flash('loginMessage'), */
+            displayName: req.user ? req.user.displayName : ''
+        });
+    }
+    else {
+        return res.redirect('/articles');
+    }
+});
+
+/* Process the Login Request */
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/articles',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+/* Show Registration Page */
+router.get('/register', function (req, res, next) {
+    if (!req.user) {
+        res.render('register', {
+            title: 'Register',
+          /*  messages: req.flash('registerMessage'),*/
+            displayName: req.user ? req.user.displayName : ''
+        });
+    }
+    else {
+        return res.redirect('/articles');
+    }
+});
+
+/* POST signup data. */
+router.post('/register', passport.authenticate('local-registration', {
+    //Success go to Profile Page / Fail go to Signup page
+    successRedirect : '/articles',
+    failureRedirect : '/register',
+    failureFlash : true
+}));
+
+
+/* Process Logout Request */
+router.get('/logout', function (req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
+
+
 
 module.exports = router;
